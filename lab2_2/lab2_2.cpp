8a4a91f2315c850aaadd918a7973b0d12689834a
catprogrammer-cat <catprogrammer.vlad@gmail.com>
2019-09-25 22:35:00 +0300

    new update of my code

--- lab2_2/lab2_2.cpp
+++ lab2_2/lab2_2.cpp
@@ -1,8 +1,38 @@
-
-
 #include <iostream>
+#include <cmath>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	double PI = 3.14159265359;
+	double E  = 2.71828182845;
+
+	double x;
+	int k;
+	cout << "Enter varible x: ";
+	cin >> x;
+	cout << endl;
+	cout << "enter what formula you want to choose 1, 2 or 3; ";
+	cin >> k;
+	cout << endl;
+	double result = atan(2 * x + 1) + 1;
+	switch (k)
+	{
+	case 1: result = atan(2 * x + 1) + 1;
+		cout << result << endl;
+		break;
+		system("pause");
+	case 2: result = cos((PI / 2) - pow(x, x) + pow(E, abs(x + 5)));
+		cout << result << endl;
+		break;
+		system("pause");
+	case 3: result = log((1 + x) / (pow(x, 3) + pow(abs(x), 1. / 2.)));
+		cout << result << endl;
+		break;
+		system("pause");
+	default:  cout << "Incorrect value of the variant!" << endl;
+		system("pause");
+	}
+	
 }
